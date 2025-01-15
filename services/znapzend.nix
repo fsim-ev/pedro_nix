60b16caa09bf70d251b0d9992950d3f83bc33af0
beo45216 <ole.bendixen@st.oth-regensburg.de>
2025-01-15 15:35:47 +0100

    moves znapzend.nix

--- services/znapzend.nix
+++ services/znapzend.nix
@@ -6,21 +6,24 @@
     pure = true;
     autoCreation = true;
     features.skipIntermediates = true;
-    zetup = {
+    zetup = let
+      defaultRemote = {
+        host = "root@fren.fsim";
+        dataset = "storage/new-backup";
+      };
+    in {
       "storage/services" = {
         plan = "1m=>1d";
         recursive = true;
 
-        destinations.remote = {
-          host = "root@fren.fsim";
-          dataset = "storage/new-backup";
-          # presend = "zfs mount -R storage/backup";
-        };
+        destinations.remote = defaultRemote;
       };
 
       "storage/services/strichliste" = {
         plan = "10d=>1h";
         dataset = "storage/new-backup";
+
+        destinations.remote = defaultRemote;
       };
     };
   };
