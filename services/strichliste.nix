7b4be719d4be61ba3e4991eb33b5358cdf3d59d6
beo45216 <ole.bendixen@st.oth-regensburg.de>
2025-01-15 14:23:31 +0100

    adds more allowed ips; tries to configure matem8; dummy secrets;

--- services/strichliste.nix
+++ services/strichliste.nix
@@ -8,17 +8,25 @@
     databaseDir = "/var/lib/strichliste/db";
   };
 
-  services.nginx.virtualHosts."strichliste.fsim-ev.de" = {
-    forceSSL = true;
-    enableACME = true;
+  services.nginx.virtualHosts =
+  let
+    host =  {
+      forceSSL = true;
+      enableACME = true;
 
-    locations."/" = {
-      proxyPass = "http://localhost:${toString config.services.strichliste.port}";
-      extraConfig = ''
-        allow 172.20.36.242/32; # fsim-room
-        allow 10.0.0.0/8; # basically all wifi
-        deny all; # no traffic from the outside world
-      '';
+      locations."/" = {
+        proxyPass = "http://localhost:${toString config.services.strichliste.port}";
+        extraConfig = ''
+          allow 172.20.36.242/32;
+          allow 172.16.0.0/16;
+          allow 10.0.0.0/8;
+          deny all; # no traffic from the outside world
+        '';
+      };
     };
+  in
+  {
+    "strichliste.fsim-ev.de" = host;
+    "mate.fsim-ev.de" = host;
   };
 }
