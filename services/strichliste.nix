7c8e2c192a7c55ce7190392c027701dda275a488
beo45216 <ole.bendixen@st.oth-regensburg.de>
2025-01-23 10:18:36 +0100

    sounds are now a nix-option

--- services/strichliste.nix
+++ services/strichliste.nix
@@ -5,9 +5,146 @@
   services.strichliste = {
     enable = true;
 
-    frontEnd = builtins.fetchTarball {
-      url = "https://git.ole.blue/ole/strichliste-frontend/raw/commit/07433011ebc3c30741de2946079b5a1ac71f8f0e/build.tar";
-      sha256 = "181ds9c22r8618ylk4fbsnbvqzxzplr16s3vx56yp5f4lidl79w7";
+    customSounds = {
+      enable = true;
+
+      depositSounds = [
+        ./strichliste/spongebob_moneten.wav
+      ];
+
+      failedSounds = [
+        ./strichliste/windows_error.wav
+      ];
+
+      withdrawSounds = [
+        ./strichliste/wobble.wav
+      ];
+
+      baselineSounds = [
+        ./strichliste/ka-ching.wav
+        ./strichliste/mario-coin.wav
+        ./strichliste/futterlucke.wav
+      ];
+
+      specificSounds = [
+        {
+          # Mio Mio Mate
+          id = 1;
+          sounds = [
+            ./strichliste/mate_01.wav
+          ];
+        }
+        {
+          # Wasser
+          id = 3;
+          sounds = [
+            ./strichliste/wasser_1.wav 
+          ];
+        }
+        {
+          # Club mate
+          id = 4;
+          sounds = [
+            ./strichliste/club_mate_1.wav
+            ./strichliste/mate_01.wav
+          ];
+        }
+        {
+          # Saftschorle
+          id = 6;
+          sounds = [
+            ./strichliste/moneyboy_orangensaft.wav
+          ];
+        }
+        {
+          # Bueno
+          id = 9;
+          sounds = [
+            ./strichliste/bueno_1.wav
+          ];
+        }
+        {
+          # Erdnüsse klein
+          id = 12;
+          sounds = [
+            ./strichliste/eier.wav
+          ];
+        }
+        {
+          # Belaste
+          id = 14;
+          sounds = [
+            ./strichliste/emotional-damage.wav
+          ];
+        }
+        {
+          # Snickers
+          id = 16;
+          sounds = [
+            ./strichliste/snickers_1.wav
+          ];
+        }
+        {
+          # Maoam
+          id = 19;
+          sounds = [
+            ./strichliste/maoam_1.wav
+          ];
+        }
+        {
+          # Mentos
+          id = 20;
+          sounds = [
+            ./strichliste/eier.wav
+          ];   
+        }
+        {
+          # Spezi
+          id = 23;
+          sounds = [
+            ./strichliste/spezifische_spezi_fischer.wav
+          ];
+        }
+        {
+          # Kaffee
+          id = 24;
+          sounds = [
+            ./strichliste/coffee.wav
+            ./strichliste/coffee_2.wav
+            ./strichliste/coffee_3.wav
+          ];         
+        }
+        {
+          # Pizza
+          id = 25;
+          sounds = [
+            ./strichliste/pizza_1.wav
+          ];
+        }
+        {
+          # Radler
+          id = 27;
+          sounds = [
+            ./strichliste/radler.wav
+          ];
+        }
+        {
+          # Mio Mio Banane
+          id = 28;
+          sounds = [
+            ./strichliste/minion_banana.wav
+            ./strichliste/mio_mio_banana_2.wav
+          ];
+        }
+        {
+          # Duplo
+          id = 30;
+          sounds = [
+            ./strichliste/duplo_1.wav
+            ./strichliste/duplo_2.wav
+          ];
+        }
+      ];
     };
 
     nginxSettings = {
