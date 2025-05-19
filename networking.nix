47499156c92338e268808e6b26d870caece3a51a
haennes <hannes.hofmuth@gmail.com>
2025-05-19 17:07:15 +0200

    move to systemd networking

--- networking.nix
+++ networking.nix
@@ -11,34 +11,61 @@
         "monolith.fsim"
       ];
     };
-
-    interfaces.eno8303 = {
-      useDHCP = false;
-      ipv4.addresses = [
-        {
-          address = "195.37.211.44";
-          prefixLength = 27;
-        }
-      ];
+    nat = {
+      enable = true;
+      externalInterface = "eno8303";
+      # internal interfaces configured in other locations respectively
     };
-
-    defaultGateway = {
-      address = "195.37.211.33";
-      interface = "eno8303";
-    };
-
-    nameservers = [ 
-      "194.95.106.120"
-      "194.95.106.121"
-    ];
-
-    enableIPv6 = false;
-  };
-
-  networking.useDHCP = false;
+   };
 
   boot.kernel.sysctl = {
     # Allow containers to access internet
     "net.ipv4.ip_forward" = 1;
   };
+
+  systemd.network = {
+    networks."10-wan" =  {
+    matchConfig.Name = "eno8303";
+    address = [
+          "195.37.211.44/27"
+    ];
+  routes = [ {Gateway = "195.37.211.33";} ];
+  dns = [ 
+      "194.95.106.120"
+      "194.95.106.121"
+  ];
+
+  linkConfig.RequiredForOnline = "routable";
+  };
+
+};
+
+networking.nat.internalInterfaces = ["br0" ];
+ systemd.network = {
+   netdevs = {
+     "20-br0" = {
+       netdevConfig = {
+         Name = "br0";
+         Kind = "bridge";
+       };
+     };
+   };
+   networks = {
+     "30-eno8303" = {
+       matchConfig.Name = "eno8303";
+       networkConfig.Bridge = "br0";
+       linkConfig.RequiredForOnline = "enslaved";
+     };
+     "40-br0" = {
+       matchConfig.Name = "br0";
+       addresses = [ { Address = "10.1.0.1/24"; } ];
+       bridgeConfig = {};
+       linkConfig.RequiredForOnline = "carrier";
+     };
+     "11-vms" = {
+        matchConfig.Name = "vm-*";
+        networkConfig.Bridge = "br0";
+     };
+   };
+ };
 }
