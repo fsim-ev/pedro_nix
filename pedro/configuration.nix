e6b930f8773af8e6c38d89ee837ea435cfd5254a
haennes <hannes.hofmuth@gmail.com>
2025-11-24 19:36:48 +0100

    fix(rebuild) up file handle limits

--- pedro/configuration.nix
+++ pedro/configuration.nix
@@ -67,4 +67,9 @@
     font = "Lat2-Terminus16";
     keyMap = "de";
   };
+
+  security.pam.loginLimits = [
+    { domain = "*"; type = "soft"; item = "nofile"; value = "65536"; }
+    { domain = "*"; type = "hard"; item = "nofile"; value = "1048576"; }
+  ];
 }
